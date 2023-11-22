5e7a34c6626495af62e4ba7a1e902e2e179f6a92
duyvu <duyvukim752000@gmail.com>
2023-11-22 20:49:52 +0700

    Create the add product function and remove product function

--- src/synchronization/producer_consumer/Buffer.java
+++ src/synchronization/producer_consumer/Buffer.java
@@ -4,10 +4,33 @@
  */
 package synchronization.producer_consumer;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author duyvu
  */
 public class Buffer {
-    
+
+    // =============================
+    // == Fields
+    // =============================
+    private int capacity;
+    private ArrayList<Integer> products = new ArrayList<>();
+
+    // =============================
+    // == Methods
+    // =============================
+    public void addProducts(int product, int producerId) {
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Produceer " + producerId + " added product " + product);
+        products.add(product);
+        System.out.println("In Stock: " + products.size());
+    }
+
+    public void removeProduct(int consumerId) {
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Consumer " + consumerId + " has bought the product " + products.get(0));
+        products.remove(0);
+    }
 }
