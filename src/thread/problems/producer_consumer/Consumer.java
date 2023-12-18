222db1280dd45ad17cbc6c30c8d4cbe3168d26f5
duyvu <duyvukim752000@gmail.com>
2023-12-18 21:08:57 +0700

    Set the longer time forthe consumer

--- src/thread/problems/producer_consumer/Consumer.java
+++ src/thread/problems/producer_consumer/Consumer.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package problems.producer_consumer;
+package thread.problems.producer_consumer;
 
 import static java.lang.Thread.sleep;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author duyvu
  */
 public class Consumer extends Thread {
@@ -25,14 +25,16 @@ public class Consumer extends Thread {
 
     @Override
     public void run() {
+
+        // Automatically take the product from the buffer continuously
         while (true) {
             try {
                 if (buffer.getSize() > 0) {
-                    buffer.removeProducts(id);
-                    sleep((long) (Math.random() * 100));
+                    buffer.removeProduct(id);
+                    sleep((long) (Math.random() * 2000));
                 }
             } catch (InterruptedException ex) {
-                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+                ex.getStackTrace();
             }
         }
     }
